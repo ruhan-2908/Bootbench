25933499483f8879a559f60a87282135072c7b15
ruhan-2908 <ruhankb29@gmail.com>
2025-10-21 15:48:18 +0530

    Added user Details

--- src/main/java/com/example/SpringBootL/config/SecurityConfig.java
+++ src/main/java/com/example/SpringBootL/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,4 +26,19 @@ public class SecurityConfig {
         ;
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailService()
+    {
+        UserDetails user = User.withUsername("alice")
+                .password("user123")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("zack")
+                 .password("admin123")
+                 .roles("ADMIN")
+                 .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
 }
