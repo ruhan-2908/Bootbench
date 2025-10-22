040cfc6b247103de0a061cdff9170ad67b65d475
ruhan-2908 <ruhankb29@gmail.com>
2025-10-22 20:18:10 +0530

    Add some user credentials

--- src/main/java/com/example/SpringBootL/config/SecurityConfig.java
+++ src/main/java/com/example/SpringBootL/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.SpringBootL.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -20,11 +21,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         http.authorizeHttpRequests(authz ->
-                    authz.requestMatchers("/api/users/**").authenticated()
+                    authz.requestMatchers(HttpMethod.POST,"/api/users").permitAll()
+                            .requestMatchers("/api/users/**").authenticated()
                             .anyRequest().permitAll()
         )
                 .formLogin(form -> form.permitAll().defaultSuccessUrl("/dashboard"))
-
+                .csrf(csrf->csrf.disable())
         ;
         return http.build();
     }
--- src/main/java/com/example/SpringBootL/entity/UserEntity.java
+++ src/main/java/com/example/SpringBootL/entity/UserEntity.java
@@ -10,10 +10,12 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public UserEntity(Long id, String name, String email) {
+    public UserEntity(Long id, String name, String email,String username, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.username = username;
+        this.password = password;
     }
     public UserEntity()
     {
@@ -46,4 +48,23 @@ public class UserEntity {
     }
 
     private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String username;
+    private String password;
 }
